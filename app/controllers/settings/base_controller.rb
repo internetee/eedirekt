6b3e084ec9c10f69c92b98a358e0e6ef83e23f20
Oleg Hasjanov <oleg.phenomenon@gmail.com>
2023-07-12 10:53:08 +0300

    fixed layout for initial settings

--- app/controllers/settings/base_controller.rb
+++ app/controllers/settings/base_controller.rb
@@ -2,6 +2,8 @@ module Settings
   class BaseController < ApplicationController
     before_action :redirect_to_admin_session
 
+    layout 'sessions'
+
     private
 
     def redirect_to_admin_session
